76b65c6c29c059f776b945a45ef001f84ec2212b
Thithiksha <thithiksha.slt@gmail.com>
2024-12-26 21:35:59 +0530

    models added

--- ticketbooking/crudapp/models.py
+++ ticketbooking/crudapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Stage_Event(models.Model):
+    name = models.CharField(max_length=45)
+    detail = models.CharField(max_length=45)
+    organizer = models.CharField(max_length=45)
+
+class Show(models.Model):
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    stage_event_id = models.ForeignKey(Stage_Event, on_delete=models.CASCADE)
+
+class Ticket(models.Model):
+    price = models.FloatField()
+    customer = models.CharField(max_length=45)
+    no_of_seats = models.IntegerField()
+    stage_event_show = models.ForeignKey(Show,on_delete=models.CASCADE)
+
+    
