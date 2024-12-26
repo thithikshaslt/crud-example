9a7430c54bc3fd0e2abd75b8a1cadfe99796c656
Thithiksha <thithiksha.slt@gmail.com>
2024-12-26 21:58:10 +0530

    registered models in admin panel

--- ticketbooking/crudapp/admin.py
+++ ticketbooking/crudapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Stage_Event,Show,Ticket
+
 # Register your models here.
+admin.site.register(Stage_Event)
+admin.site.register(Show)
+admin.site.register(Ticket)
